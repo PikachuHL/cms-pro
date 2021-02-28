932bf8a0931b8860672cb2b59bc3ed1fdf7d4ab2
PikachuHL <1262637008@qq.com>
2021-02-28 18:21:36 +0800

    fix redirect loginPage

--- cms_portal/src/main/java/cn/hellopika/portal/security/filter/CmsUserFilter.java
+++ cms_portal/src/main/java/cn/hellopika/portal/security/filter/CmsUserFilter.java
@@ -3,6 +3,11 @@ package cn.hellopika.portal.security.filter;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.shiro.web.filter.authc.UserFilter;
+import org.apache.shiro.web.util.WebUtils;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 @Setter
 @Getter
@@ -11,5 +16,19 @@ public class CmsUserFilter extends UserFilter {
     private String adminLoginUrl;
     private String adminPrefix;
 
+    @Override
+    protected boolean onAccessDenied(ServletRequest req, ServletResponse resp) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) req;
+        this.saveRequest(request);
 
+        // 如果是后台链接, 则跳转到后台登录界面, 否则跳转到前台登录界面
+        if(request.getRequestURI().startsWith(adminPrefix)){
+            WebUtils.issueRedirect(request, resp, adminLoginUrl);
+        }else{
+            WebUtils.issueRedirect(request, resp, this.getLoginUrl());
+        }
+
+
+        return false;
+    }
 }
